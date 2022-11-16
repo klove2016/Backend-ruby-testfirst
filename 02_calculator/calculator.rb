97de8366b157059fedb470690e959dc9718588d6
KJ <kjloving1400@gmail.com>
2022-11-16 00:51:03 -0800

    all tests passing need to finish strech exercises on calculator program

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,14 @@
 #write your code here
+def add(a, b)
+    a + b
+end
+def subtract(a, b)
+    a - b
+end
+def sum(array)
+    sum = 0; 
+        for num in 0..array.length
+            sum += array[num].to_i;
+        end
+    return sum;
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,32 @@
 #write your code here
+def translate(words)
+    split_words = words.split(' ')
+    split_words.map {|word| translate_one_word(word)}.join(' ')
+
+end
+
+def translate_one_word(word)
+    firstlet = word[0]
+ seclet = word[1]
+ thirdlet = word[2]
+ vowel = ['a','e','i','o','u']
+   
+        if firstlet + seclet == 'qu'
+            word.slice!(0,2)
+            word.insert(-1, "#{firstlet}#{seclet}ay")
+        elsif (seclet + thirdlet == 'qu') || ( firstlet + seclet + thirdlet == 'sch')
+            word.slice!(0,3)
+            word.insert(-1, "#{firstlet}#{seclet}#{thirdlet}ay")
+        elsif vowel.include?(firstlet) 
+            word.insert(-1, "ay")
+       elsif !vowel.include?(firstlet) && !vowel.include?(seclet) && !vowel.include?(thirdlet)
+            word.slice!(0,3)
+            word.insert(-1, "#{firstlet}#{seclet}#{thirdlet}ay") 
+        elsif !vowel.include?(firstlet) && !vowel.include?(seclet)
+            word.slice!(0,2)
+            word.insert(-1, "#{firstlet}#{seclet}ay")
+        else 
+            word.slice!(0)
+            word.insert(-1, "#{firstlet}ay")
+        end
+end
