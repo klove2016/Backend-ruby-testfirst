72307a1f456c9cadcdca096244fb0edb21997e2d
KJ <101077648+klove2016@users.noreply.github.com>
2022-11-29 13:53:01 -0800

    Update 05_book_titles/book.rb
    Co-authored-by: Daniel Sinn <dsinn@pagerduty.com>

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -2,7 +2,7 @@ class Book
 # write your code here
 attr_accessor :title
     def title
-        no_words = ['and','of','in','an', 'the','a']
+        little_words = ['and','of','in','an', 'the','a']
             @title.capitalize.split(" ").map {|word| 
             if no_words.include?(word) 
                 #puts word
